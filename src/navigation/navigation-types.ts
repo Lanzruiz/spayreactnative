b9f08377cd74f8ee3697d56de425cac185cf3423
NextLogic DevOps <system.admin@philwebservices.com>
2023-07-26 22:16:10 +0800

    initial commit

---
+++ src/navigation/navigation-types.ts
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  SplashScreen: undefined;
+  Crypto: undefined;
+};
+export type CryptoStackParamList = {
+  CryptoIntro: undefined;
+  Crypto01: undefined;
+  Crypto02: undefined;
+  Crypto03: undefined;
+  Crypto04: undefined;
+  Crypto05: undefined;
+  Crypto06: undefined;
+  Crypto07: undefined;
+  Crypto08: undefined;
+  Crypto09: undefined;
+  Crypto10: undefined;
+};
